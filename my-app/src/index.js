c1683970dd15785fddab0094501f39c26088a992
김민균 <dirak4545@gmail.com>
2024-05-10 23:19:39 +0900

    d

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,8 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <Library />
     <Clock />
+    <CommentList />
+    <NotificationList />
     <Accomodate />
+    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>
 );
 
